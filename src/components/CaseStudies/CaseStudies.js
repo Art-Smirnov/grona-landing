3756a0b265eaa433e3e7f3e6a29224738c4e7c1b
Art-Smirnov <artem@classtag.com>
2023-05-01 17:13:18 +0300

    add diferent screens to CaseStudies

--- src/components/CaseStudies/CaseStudies.js
+++ src/components/CaseStudies/CaseStudies.js
@@ -1,83 +1,108 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel, Navigation } from 'swiper';
-import projectCover from '../images/project1_cover.png';
+import projectCover from '../images/project-cover.png';
+import { ReactComponent as ArrowIcon } from '../images/arrow-forward.svg';
+import useScreen from '../hooks/useScreen';
 
 import 'swiper/swiper-bundle.css';
 import './CaseStudies.sass';
 
 const CaseStudies = () => {
+  const { isMobile, screenWidth } = useScreen(575);
+  const isVertical = isMobile() ? 'vertical' : 'horizontal';
+  const isThreePreviews = screenWidth >= 1024;
+
   return (
     <div id="case-studies" className="container case-studies">
-      <div className="case-studies-titles">
+      <div className="case-studies-subtitles">
         <span className="case-studies-quantity">1 &mdash; 5</span>
-        <h2 className="case-studies-title">Case Studies</h2>
+        <ArrowIcon />
       </div>
-      <Swiper
-        className="case-studies-swiper"
-        modules={[Navigation, Mousewheel]}
-        spaceBetween={50}
-        slidesPerView={2}
-        navigation
-        mousewheel={true}
-        grabCursor={true}
-        // onSwiper={(swiper) => console.log(swiper)}
-        // onSlideChange={() => console.log('slide change')}
-      >
-        <SwiperSlide className="case-studies-project-slide">
-          <div className="case-studies-project-slide-container">
-            <img
-              className="case-studies-project-slide-image"
-              src={projectCover}
-              alt="Project cover"
-            />
-            <span>Project 1</span>
-          </div>
-        </SwiperSlide>
+      <div className="case-studies-content">
+        <h2 className="case-studies-title">Case Studies</h2>
+        <Swiper
+          className="case-studies-swiper"
+          modules={[Navigation, Mousewheel]}
+          spaceBetween={8}
+          slidesPerView={isThreePreviews ? 3 : 2}
+          // navigation
+          mousewheel={true}
+          grabCursor={true}
+          direction={isVertical}
+          // onSwiper={(swiper) => console.log(swiper)}
+          // onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide className="case-studies-project-slide">
+            <div className="case-studies-project-slide-container">
+              <img
+                className="case-studies-project-slide-image"
+                src={projectCover}
+                alt="Project cover"
+              />
+              <span className="case-studies-project-slide-subtitle">
+                UX/UI design
+              </span>
+              <span className="case-studies-project-slide-title">Polestar</span>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="case-studies-project-slide">
-          <div className="case-studies-project-slide-container">
-            <img
-              className="case-studies-project-slide-image"
-              src={projectCover}
-              alt="Project cover"
-            />
-            <span>Project 2</span>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide className="case-studies-project-slide">
+            <div className="case-studies-project-slide-container">
+              <img
+                className="case-studies-project-slide-image"
+                src={projectCover}
+                alt="Project cover"
+              />
+              <span className="case-studies-project-slide-subtitle">
+                UX/UI design
+              </span>
+              <span className="case-studies-project-slide-title">Polestar</span>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="case-studies-project-slide">
-          <div className="case-studies-project-slide-container">
-            <img
-              className="case-studies-project-slide-image"
-              src={projectCover}
-              alt="Project cover"
-            />
-            <span>Project 3</span>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide className="case-studies-project-slide">
+            <div className="case-studies-project-slide-container">
+              <img
+                className="case-studies-project-slide-image"
+                src={projectCover}
+                alt="Project cover"
+              />
+              <span className="case-studies-project-slide-subtitle">
+                UX/UI design
+              </span>
+              <span className="case-studies-project-slide-title">Polestar</span>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="case-studies-project-slide">
-          <div className="case-studies-project-slide-container">
-            <img
-              className="case-studies-project-slide-image"
-              src={projectCover}
-              alt="Project cover"
-            />
-            <span>Project 4</span>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide className="case-studies-project-slide">
+            <div className="case-studies-project-slide-container">
+              <img
+                className="case-studies-project-slide-image"
+                src={projectCover}
+                alt="Project cover"
+              />
+              <span className="case-studies-project-slide-subtitle">
+                UX/UI design
+              </span>
+              <span className="case-studies-project-slide-title">Polestar</span>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="case-studies-project-slide">
-          <div className="case-studies-project-slide-container">
-            <img
-              className="case-studies-project-slide-image"
-              src={projectCover}
-              alt="Project cover"
-            />
-            <span>Project 5</span>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+          <SwiperSlide className="case-studies-project-slide">
+            <div className="case-studies-project-slide-container">
+              <img
+                className="case-studies-project-slide-image"
+                src={projectCover}
+                alt="Project cover"
+              />
+              <span className="case-studies-project-slide-subtitle">
+                UX/UI design
+              </span>
+              <span className="case-studies-project-slide-title">Polestar</span>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
