b2f0e9bcad73fd11ae26ca1d5fca95cb5f222c61
Art-Smirnov <artem@classtag.com>
2023-06-15 16:19:33 +0300

    draft

--- src/index.js
+++ src/index.js
@@ -18,25 +18,42 @@ const router = createBrowserRouter([
     //     element: <CaseModal />,
     //   },
     // ],
-    children: [
-      {
-        path: 'cases/bbq-box',
-        element: <span>!!!!!!!!!!!!!!</span>,
-        // element: <CaseModal />,
-      },
-      {
-        path: 'cases/wizcric',
-        element: <CaseModal />,
-      },
-      {
-        path: 'cases/kram',
-        element: <CaseModal />,
-      },
-      {
-        path: 'cases/polestar',
-        element: <CaseModal />,
-      },
-    ],
+    // children: [
+    //   {
+    //     path: 'cases/bbq-box',
+    //     element: <span>!!!!!!!!!!!!!!</span>,
+    //     // element: <CaseModal />,
+    //   },
+    //   {
+    //     path: 'cases/wizcric',
+    //     element: <CaseModal />,
+    //   },
+    //   {
+    //     path: 'cases/kram',
+    //     element: <CaseModal />,
+    //   },
+    //   {
+    //     path: 'cases/polestar',
+    //     element: <CaseModal />,
+    //   },
+    // ],
+  },
+  {
+    path: 'cases/bbq-box',
+    element: <span>!!!!!!!!!!!!!!</span>,
+    // element: <CaseModal />,
+  },
+  {
+    path: 'cases/wizcric',
+    element: <CaseModal />,
+  },
+  {
+    path: 'cases/kram',
+    element: <CaseModal />,
+  },
+  {
+    path: 'cases/polestar',
+    element: <CaseModal />,
   },
   // {
   //   path: 'cases/:caseName',
